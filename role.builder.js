daac666b83b2a4c414af521d46c49b11326b003c
yaz9900 <36150651+yaz9900@users.noreply.github.com>
2018-02-05 23:57:20 -0800

    updated again

--- role.builder.js
+++ role.builder.js
@@ -14,16 +14,18 @@ var roleBuilder = {
 
 	    if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+	        //console.log(targets)
             if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+				buildTarget = targets[Math.min.apply( Math, foreach(sqrt((targets[].pos.x-this.pos.x)^2 + (targets[].pos.y-this.pos.y)^2)))] ;
+                if(creep.build(buildtarget) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
 	    }
 	    else {
 	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[creep.memory.source], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
 	    }
 	}
