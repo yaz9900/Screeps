ddf83ad6fc14b39cdd526ed4cf2ea9d42fa1aec5
yaz9900 <36150651+yaz9900@users.noreply.github.com>
2018-02-05 23:53:51 -0800

    updated builder to work on closest tile

--- role.builder.js
+++ role.builder.js
@@ -14,6 +14,7 @@ var roleBuilder = {
 
 	    if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+	        //console.log(targets)
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
@@ -22,11 +23,14 @@ var roleBuilder = {
 	    }
 	    else {
 	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[creep.memory.source], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
 	    }
 	}
 };
 
 module.exports = roleBuilder;
+
+
+//Testing
